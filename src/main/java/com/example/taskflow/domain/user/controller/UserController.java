e5146cf05d7cf098382262a22b3b004dabe9ce02
i-am-dahee <hxxly02@gmail.com>
2025-12-09 16:58:55 +0900

    feat: Validation 적용

--- src/main/java/com/example/taskflow/domain/user/controller/UserController.java
+++ src/main/java/com/example/taskflow/domain/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.taskflow.domain.user.controller;
 import com.example.taskflow.domain.user.dto.request.UserCreateRequestDto;
 import com.example.taskflow.domain.user.dto.response.UserCreateResponseDto;
 import com.example.taskflow.domain.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class UserController {
 
     // 회원가입
     @PostMapping("/api/users")
-    public ResponseEntity<UserCreateResponseDto> createUser(@RequestBody UserCreateRequestDto request){
+    public ResponseEntity<UserCreateResponseDto> createUser(@Valid @RequestBody UserCreateRequestDto request){
         UserCreateResponseDto result = userService.createUser(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
--- src/main/java/com/example/taskflow/domain/user/dto/request/UserCreateRequestDto.java
+++ src/main/java/com/example/taskflow/domain/user/dto/request/UserCreateRequestDto.java
@@ -1,12 +1,30 @@
 package com.example.taskflow.domain.user.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserCreateRequestDto {
 
+    @NotBlank(message = "아이디는 필수 입력값입니다.")
+    @Size(min = 4, max = 20, message = "아이디는 4~20자여야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "아이디는 영문과 숫자만 사용할 수 있습니다.")
     private String username;
+
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email(message = "유효한 이메일 형식이 아닙니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).+$",
+             message = "비밀번호는 영문, 숫자, 특수문자를 모두 포함해야 합니다.")
     private String password;
+
+    @NotBlank(message = "이름은 필수 입력값입니다.")
+    @Size(min = 2, max = 50, message = "이름은 2~50자여야 합니다.")
     private String name;
 }
