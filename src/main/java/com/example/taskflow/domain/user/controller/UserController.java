800fc58be287e98ed2fbb3a731e5a09a75801f75
i-am-dahee <hxxly02@gmail.com>
2025-12-09 21:47:12 +0900

    feat: 사용자 목록 조회 기능

--- src/main/java/com/example/taskflow/domain/user/controller/UserController.java
+++ src/main/java/com/example/taskflow/domain/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.taskflow.domain.user.controller;
 import com.example.taskflow.common.response.GlobalResponse;
 import com.example.taskflow.domain.user.dto.request.UserCreateRequestDto;
 import com.example.taskflow.domain.user.dto.response.UserCreateResponseDto;
+import com.example.taskflow.domain.user.dto.response.UserGetAllResponseDto;
 import com.example.taskflow.domain.user.dto.response.UserGetResponseDto;
 import com.example.taskflow.domain.user.service.UserService;
 import jakarta.validation.Valid;
@@ -10,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,5 +37,14 @@ public class UserController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(GlobalResponse.success(true, "사용자 정보 조회가 완료되었습니다.", result));
+    }
+
+    // 사용자 목록 조회
+    @GetMapping("/api/users")
+    public ResponseEntity<GlobalResponse<List<UserGetAllResponseDto>>> getAllUsers() {
+        List<UserGetAllResponseDto> result = userService.getAllUsers();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(GlobalResponse.success(true, "사용자 목록 조회가 완료되었습니다.", result));
     }
 }
