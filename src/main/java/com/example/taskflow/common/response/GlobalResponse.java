f40a786da0f2cb5419e1513d3e8c41870b186125
i-am-dahee <hxxly02@gmail.com>
2025-12-09 18:19:29 +0900

    refactor: 공통 응답 형식 수정

--- src/main/java/com/example/taskflow/common/response/GlobalResponse.java
+++ src/main/java/com/example/taskflow/common/response/GlobalResponse.java
@@ -5,13 +5,22 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
-@AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
+//@JsonInclude(JsonInclude.Include.NON_NULL)
 public class GlobalResponse<T> {
     private boolean success;
     private String message;
     private T data;
+    private LocalDateTime timestamp;
+
+    public GlobalResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+        this.timestamp = LocalDateTime.now();
+    }
 
     //성공시
     public static <T> GlobalResponse<T> success(boolean success, String message, T data) {
