fdb934b844bcd4f26f0b0aac5cf733196f853678
i-am-dahee <hxxly02@gmail.com>
2025-12-10 20:38:02 +0900

    refactor: 필요 없는 코드 제거

--- src/main/java/com/example/taskflow/domain/comment/service/CommentService.java
+++ src/main/java/com/example/taskflow/domain/comment/service/CommentService.java
@@ -20,9 +20,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
