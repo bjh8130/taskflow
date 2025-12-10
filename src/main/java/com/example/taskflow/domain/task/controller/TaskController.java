3ebcd476b91774c36514e5f8254f2010a69cca1e
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2025-12-10 09:18:24 +0900

    refactor: 변수명 통일

--- src/main/java/com/example/taskflow/domain/task/controller/TaskController.java
+++ src/main/java/com/example/taskflow/domain/task/controller/TaskController.java
@@ -20,10 +20,10 @@ public class TaskController {
     @PostMapping()
     public ResponseEntity<GlobalResponse<TaskCreateResponseDto>> createTask(@Valid @RequestBody TaskCreateRequestDTO request) {
 
-        TaskCreateResponseDto response = taskService.createTask(request);
+        TaskCreateResponseDto result = taskService.createTask(request);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(GlobalResponse.success(true, "Task 생성 성공",response));
+                .body(GlobalResponse.success(true, "Task 생성 성공", result));
 
     }
 }
