97f3457d2ed7e060b2a83ebdb144260073dec894
i-am-dahee <hxxly02@gmail.com>
2025-12-10 10:59:42 +0900

    refactor: 필요 없는 코드 제거

--- src/main/java/com/example/taskflow/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/taskflow/domain/user/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package com.example.taskflow.domain.user.repository;
 
 import com.example.taskflow.domain.user.entity.User;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
