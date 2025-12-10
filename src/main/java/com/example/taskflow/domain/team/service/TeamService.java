eb436086270634a39a52202965d08fb77228655e
Thugi-98 <kimjh4533@gmail.com>
2025-12-10 15:01:44 +0900

    feat: Team 목록 조회, 상세 조회 구현

--- src/main/java/com/example/taskflow/domain/team/service/TeamService.java
+++ src/main/java/com/example/taskflow/domain/team/service/TeamService.java
@@ -4,6 +4,7 @@ import com.example.taskflow.common.exception.CustomException;
 import com.example.taskflow.common.exception.ErrorCode;
 import com.example.taskflow.domain.team.dto.request.TeamCreateRequestDto;
 import com.example.taskflow.domain.team.dto.response.TeamCreateResponseDto;
+import com.example.taskflow.domain.team.dto.response.TeamReadResponseDto;
 import com.example.taskflow.domain.team.entity.Team;
 import com.example.taskflow.domain.teamMember.repository.TeamMemberRepository;
 import com.example.taskflow.domain.team.repository.TeamRepository;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -33,5 +35,28 @@ public class TeamService {
         List<User> members = teamMemberRepository.findUserIdByTeamId(team.getId());
 
         return new TeamCreateResponseDto(team.getId(), team.getName(), team.getDescription(), team.getCreatedAt(), members);
+    }
+
+    @Transactional(readOnly = true)
+    public List<TeamReadResponseDto> findAll() {
+        List<Team> teams = teamRepository.findAll();
+        List<TeamReadResponseDto> dtos = new ArrayList<>();
+
+        for (Team team : teams) {
+            List<User> members = teamMemberRepository.findUserIdByTeamId(team.getId());
+            dtos.add(new TeamReadResponseDto(team.getId(), team.getName(), team.getDescription(), team.getCreatedAt(), members));
+        }
+
+        return dtos;
+    }
+
+    @Transactional(readOnly = true)
+    public TeamReadResponseDto findOne(Long id) {
+        Team team = teamRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.TEAM_NOT_FOUND));
+
+        List<User> members = teamMemberRepository.findUserIdByTeamId(team.getId());
+
+        return new TeamReadResponseDto(team.getId(), team.getName(), team.getDescription(), team.getCreatedAt(), members);
     }
 }
