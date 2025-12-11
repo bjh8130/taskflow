90887ff6951dcc4eb4e0197ab1938f6e2aeb37f3
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2025-12-11 14:12:57 +0900

    ♻ refactor: 주석 삭제

--- src/main/java/com/example/taskflow/search/controller/SearchController.java
+++ src/main/java/com/example/taskflow/search/controller/SearchController.java
@@ -22,23 +22,3 @@ public class SearchController {
                 .body(GlobalResponse.success(true, "검색 성공.", result));
     }
 }
-/*
-검색어(query)1개로 Task,Team,User 각각 검색
-->하나의 응답으로
-
-검색기능 아키텍쳐 흐름
-SearchController -> SearchService -> 각 도메인 Repository -> 검색 결과 DTO -> GlobalResponse
-
-SearchController
-- query 파라미터 검증
-    - query null ? -> 400
-    - query 공백 ? -> 400
-- searchService 호출
-
-SearchService
-- 검색을 할 각 도메인의 레포지토리 호출
-- 각 검색 결과 엔티티 리스트를 DTO로 변환
-
-SearchResponseDTO
-
- */
